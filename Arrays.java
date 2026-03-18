61ff776e77dfe5ff2c07e637bdc414baff51190e
krushnashende80 <krushnashende80@gmail.com>
2026-03-18 23:37:01 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -4,7 +4,7 @@ public class Arrays {
        arr[0]=new int[]{11,12,13,14};   
        arr[1]=new int[]{15,16,17,18};
        arr[2]=new int[]{19,20,21,22};
-
+  
        for(int i=0;i<3;i++){
            for(int j=0;j<arr[i].length;j++){
                System.out.print(arr[i][j]+" ");
