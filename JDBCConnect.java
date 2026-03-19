ae0eb05c27d14307bfd8bb7746414e4d00711245
krushnashende80 <krushnashende80@gmail.com>
2026-03-19 23:56:21 +0530

    Update JDBCConnect.java

--- JDBCConnect.java
+++ JDBCConnect.java
@@ -4,7 +4,7 @@ public class JDBCConnect {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
+  
             Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/testdb",
                 "root",
