fc970b5e3ac9995a35fb03e3c480f485622f4126
krushnashende80 <krushnashende80@gmail.com>
2026-04-10 23:59:20 +0530

    Refactor 2D array initialization and printing logic

--- Arrays.java
+++ Arrays.java
@@ -5,7 +5,7 @@ public class Arrays {
        arr[1]=new int[]{15,16,17,18};
        arr[2]=new int[]{19,20,21,22};
      
-       for(int i=0;i<3;i++){   
+       for(int i=0;i<3;i++){      
            for(int j=0;j<arr[i].length;j++){
                System.out.print(arr[i][j]+" ");  
 
