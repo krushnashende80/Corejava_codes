a28de2b71554e0d2f33ff826df6b50875ee6af7e
krushnashende80 <krushnashende80@gmail.com>
2026-04-21 23:27:04 +0530

    Update HCF.java

--- HCF.java
+++ HCF.java
@@ -4,7 +4,7 @@ public class HCF {
 
         int hcf = getHCF(num1,num2);               
         System.out.println(" THE HCF IS " + hcf);      
-  
+     
         int lcm = (num1 * num2) / hcf;      
         System.out.println(" The LCM number is " + lcm);
 
