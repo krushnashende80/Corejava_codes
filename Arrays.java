e7c528fa18ccfa1596a1abd954573fd141a04bbe
krushnashende80 <krushnashende80@gmail.com>
2026-05-11 23:40:23 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -29,7 +29,7 @@ public class Arrays{
        for(int i=0;i<arr.length;i++){
            for(int j=0;j<arr[i].length;j++){
                System.out.print(arr[i][j] + " ");
-           }
+           } 
            System.out.println();
        }
    }
