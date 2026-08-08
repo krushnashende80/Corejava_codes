2d8f6ecf23560d46cac6608e33a339e24f27a01f
jayyy9018 <jayyy9018@gmail.com>
2026-08-08 10:39:32 +0530

    Adding java program

---
+++ start.java
@@ -0,0 +1,9 @@
+public class start{
+ 
+    public static void main(String[] args) {
+        String str = "KRUSHNA";
+        for(int i = 0; i < str.length(); i++){
+         System.out.print(str.charAt(i));
+        }
+    }
+}
